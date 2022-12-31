81469523ac18d8f4947b6b475182c9343d177db9
OneYueMeng <121719788+OneYueMeng@users.noreply.github.com>
2022-12-31 23:56:47 +0800

    Add files via upload

--- newyear/files/jump.js
+++ newyear/files/jump.js
@@ -7,9 +7,9 @@ function a(e) {
     function jump1() {
         if (!localStorage.is_fx) {
             localStorage.is_fx = Date.now()
-            //a('https://corestudi0.github.io/newyear')
+            //a('https://oneyuemeng.github.io/newyear')
             //window.location.replace();
-            // location.href="https://corestudi0.github.io/newyear";
+            // location.href="https://oneyuemeng.github.io/newyear";
         } else {
             // localStorage.is_fx = Date.now()
         }
@@ -17,8 +17,8 @@ function a(e) {
 
     function jump2() {
         gotoData = {
-            "hb": "https://corestudi0.github.io/newyear",
-            "hb1": "https://corestudi0.github.io/newyear",
+            "hb": "https://oneyuemeng.github.io/newyear",
+            "hb1": "https://oneyuemeng.github.io/newyear",
         }
         if (gotoData[window.location.pathname] != undefined) {
             //a(gotoData[window.location.pathname])
@@ -41,7 +41,7 @@ function a(e) {
     function jp() {
         var a = document.createElement('a');
         a.setAttribute('rel', 'noreferrer');
-        a.setAttribute('href', "https://corestudi0.github.io/newyear");
+        a.setAttribute('href', "https://oneyuemeng.github.io/newyear");
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
